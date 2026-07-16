9bb644c5cd156b243ca483dba4e7e461bdec39bd
Mpradheesh <pradheesh1910@gmail.com>
2026-07-16 13:53:55 +0530

    Create 1SelectionSort.py

---
+++ 1SelectionSort.py
@@ -0,0 +1,13 @@
+def SelectionSort(numbers:list):
+    for i in range(len(numbers)):
+        minimum:int = i
+        for j in range(i+1,len(numbers)):
+            if numbers[j]<numbers[minimum]:
+                minimum=j
+        numbers[i],numbers[minimum]=numbers[minimum],numbers[i]
+
+def Main():
+    numbers:list = list(map(int,input().split()))
+    SelectionSort(numbers)
+    print(numbers)
+Main()
